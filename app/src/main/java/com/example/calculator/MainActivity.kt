9bc8fde1ea5bbc83144126d8e272cd08755ec33b
Vavara <nicklekitty1@gmail.com>
2026-02-08 14:26:40 -0800

    First Commit; Challenge 1 complete

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var currentVal = 42
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        updateCalcput()
+    }
+    fun updateCalcput(){
+        val calcput = findViewById<TextView>(R.id.calcDisplay)
+        calcput.text = currentVal.toString()
+    }
+    fun clearCalcput(){
+        currentVal = 0
+        updateCalcput()
+
     }
 }
